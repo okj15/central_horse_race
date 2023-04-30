01a84027a12038fb0c2797e7874c0a1d26e03e1c
okj15 <sh.okj15@gmail.com>
2023-05-01 03:47:18 +0900

    ・レース情報とレース結果の表示

--- frontend/components/organisms/raceInfo.tsx
+++ frontend/components/organisms/raceInfo.tsx
@@ -1,10 +1,10 @@
-import {RaceInfoType} from "@/models/result";
+import {RaceInfoType} from "@/models/raceInfo";
 
 function RaceInfo(props: {raceInfo: RaceInfoType | null}) {
     return (
         <div>
             <h1>RaceInfo</h1>
-            <p>開催日: {props.raceInfo?.raceDate.toDateString()}</p>
+            <p>開催日: {props.raceInfo?.raceDate.toLocaleDateString()}</p>
             <p>開催場所: {props.raceInfo?.venue}</p>
             <p>{props.raceInfo?.trackType}{props.raceInfo?.direction}{props.raceInfo?.distance}</p>
             <p>{props.raceInfo?.weather}</p>
